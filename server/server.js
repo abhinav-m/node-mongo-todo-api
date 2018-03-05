774fb5222ef2a8b1d9a0e3541ab9e87593e550c7
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-05 19:46:47 +0530

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,6 +55,25 @@ app.get("/todos/:id", (req, res) => {
   );
 });
 
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then(
+    todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.send({ todo });
+    },
+    e => {
+      res.status(400).send();
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
 });
