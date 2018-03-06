ffce39a08f920f64b11949ff811e7dba8cf97116
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-06 18:05:55 +0530

    Hashed user password before saving in mongoose

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,39 +1,55 @@
-const { SHA256 } = require("crypto-js");
+const bcrypt = require("bcryptjs");
 
-const jwt = require("jsonwebtoken");
+var password = "123abcd";
 
-var data = {
-  id: 10
-};
+//Number of rounds to make the password.
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
+var hashedpass = "$2a$10$d63YJyQZaDnPN6CHp5wUtOmdVzX.SCz1UXIVangkLIUdbLwleeh1O";
 
-var decoded = jwt.verify(token, "123abc1");
-console.log("DECODED", decoded);
+bcrypt.compare(password, hashedpass, (err, res) => {
+  console.log(res);
+});
+// const { SHA256 } = require("crypto-js");
 //
-// const message = "I am user number 3";
-//
-// const hash = SHA256(message).toString();
-//
-// console.log(`Message:${message}\n Hash:${hash}`);
+// const jwt = require("jsonwebtoken");
 //
 // var data = {
-//   id: 4
+//   id: 10
 // };
 //
-// var token = {
-//   data,
-//   hash: SHA256(JSON.stringify(data) + "somesecret").toString() // "somesecret" is the salt here.
-// };
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
 //
-// //If  we change value of data and hash , data changes since the person doesn't have salt.
+// var decoded = jwt.verify(token, "123abc1");
+// console.log("DECODED", decoded);
 // //
-// // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(data).toString());
-//
-// //Salt -> Add some extra info to the data needed to be hashed, so it is more secure.
-// //Now even if data is tampered with , The value of SALT will not be known and thus, the hash will be secure.
-// var resultHash = SHA256(JSON.stringify(token.data) + "somesecret").toString();
-//
-// console.log(resultHash === token.hash ? "data not changed" : "data changed");
+// // const message = "I am user number 3";
+// //
+// // const hash = SHA256(message).toString();
+// //
+// // console.log(`Message:${message}\n Hash:${hash}`);
+// //
+// // var data = {
+// //   id: 4
+// // };
+// //
+// // var token = {
+// //   data,
+// //   hash: SHA256(JSON.stringify(data) + "somesecret").toString() // "somesecret" is the salt here.
+// // };
+// //
+// // //If  we change value of data and hash , data changes since the person doesn't have salt.
+// // //
+// // // token.data.id = 5;
+// // // token.hash = SHA256(JSON.stringify(data).toString());
+// //
+// // //Salt -> Add some extra info to the data needed to be hashed, so it is more secure.
+// // //Now even if data is tampered with , The value of SALT will not be known and thus, the hash will be secure.
+// // var resultHash = SHA256(JSON.stringify(token.data) + "somesecret").toString();
+// //
+// // console.log(resultHash === token.hash ? "data not changed" : "data changed");
