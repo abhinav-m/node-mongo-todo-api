ca671d50649a45a8a9d006fd944a3b5bc9252e93
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-06 21:55:47 +0530

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,24 @@ UserSchema.statics.findByToken = function(token) {
     "tokens.access": "auth"
   });
 };
+
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    } else
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (err || !res) reject();
+          else {
+            resolve(user);
+          }
+        });
+      });
+  });
+};
 //Mongoose middleware to run some code before saving of a user.
 UserSchema.pre("save", function(next) {
   var user = this;
